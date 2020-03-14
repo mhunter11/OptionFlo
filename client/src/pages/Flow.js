cb7cd3178b14bba8185f78c1b272b6afb75d7c9d
Eddie Penta <ecp4224@gmail.com>
2020-03-14 14:31:05 -0400

    Implemented data server for options

--- client/src/pages/Flow.js
+++ client/src/pages/Flow.js
@@ -1,28 +1,51 @@
-import React, { useEffect } from 'react'
+import React from 'react';
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import TechnicalAnalysis from 'react-tradingview-technical-analysis'
+import TechnicalAnalysis from 'react-tradingview-technical-analysis';
+import socketIOClient from "socket.io-client";
 
 import styles from './Home.module.css'
 
+export default class Flow extends React.Component {
 
-export default function Flow() {
-  useEffect(() => {
-    document.getElementsByClassName("tradingview-widget-container")[0].style.margin = "auto"
-  }, [])
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: []
+    }
+  }
 
-  const USE_SYMBOL = 'VXX'
-  return (
-    <div>
+  
+  componentDidMount() {
+    document.getElementsByClassName("tradingview-widget-container")[0].style.margin = "auto";
+
+    const socket = socketIOClient('http://localhost:8080');
+
+    socket.on("all_options", data => this.setState({ options: data }));
+    socket.on("options", data => this.setState({ options: this.state.options.unshift(...data) }));
+  }
+  
+  render() {
+    let elements = this.state.options.map(e => <li>{e.ticker}</li> );
+
+    const USE_SYMBOL = 'VXX'
+    return (
       <div>
-        <TechnicalAnalysis
-          symbol={USE_SYMBOL}
-          className={styles.container}
-        />
-        <TradingViewWidget
-          symbol={USE_SYMBOL}
-          theme={Themes.DARK}
-        />
+        <div>
+          <TechnicalAnalysis
+            symbol={USE_SYMBOL}
+            className={styles.container}
+          />
+          <TradingViewWidget
+            symbol={USE_SYMBOL}
+            theme={Themes.DARK}
+          />
+
+          <ul>
+            {elements}
+          </ul>
+
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
---
+++ data-server/index.js
@@ -0,0 +1,106 @@
+const xml2js = require('xml2js');
+const fetch = require('node-fetch');
+const date = require('date-and-time');
+const io = require('socket.io')(8080);
+
+//Date Config
+const parser = new xml2js.Parser(/* options */);
+const date_pattern = date.compile('YYYY-MM-DD HH:mm:ss');
+
+//API Config
+const API = 'https://api.benzinga.com/api/v1/signal/option_activity?apiKey=3085d152a4124662836f7f0d963672ca&token=3085d152a4124662836f7f0d963672ca';
+
+
+//Data
+var lastOptionID = null;
+const allData = [];
+
+
+function formatData(data) {
+    data = data.map(e => {
+        const keys = Object.keys(e);
+
+        for (let i = 0; i < keys.length; i++) {
+            e[keys[i]] = e[keys[i]][0];
+        }
+
+        return e;
+    });
+
+    data = data.map(e => {
+        const keys = Object.keys(e);
+
+        for (let i = 0; i < keys.length; i++) {
+            switch (keys[i]) {
+                case 'date': {
+                    const parsedDate = date.parse(e['date'] + ' ' + e['time'], date_pattern);
+                    e['date'] = parsedDate;
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+
+        return e;
+    });
+
+    return data;
+}
+
+async function fetchLatest() {
+    const response = await fetch(API);
+    const xml = await response.text();
+
+    const parsedData = await parser.parseStringPromise(xml);
+
+    let all_options = parsedData.result.option_activity[0].item;
+
+    all_options = formatData(all_options);
+
+    let new_options = [];
+    if (lastOptionID != null) {
+        for (let i = 0; i < all_options.length; i++) {
+            let id = all_options[i].id;
+
+            if (id == lastOptionID) {
+                break;
+            }
+
+            new_options.push(all_options[i]);
+        }
+    } else {
+        new_options = all_options;
+    }
+
+    if (new_options.length == 0) {
+        return false;
+    } else {
+        lastOptionID = new_options[0].id;
+
+        allData.unshift(...new_options);
+
+        //TODO Store old elements (last N) in a database (Mongo? Elasticsearch?)
+        /*if (allData.length > N) {
+            let oldElements = allData[:N];
+
+            pushToDatabase(oldElements);
+
+            allData.remove(oldElements);
+        }*/
+
+        return new_options;
+    }
+}
+
+setTimeout(function() {
+    fetchLatest().then(function(result) {
+        if (result) {
+            io.emit('options', result);
+        }
+    })
+}, 5*1000);
+
+io.on('connection', function(socket) {
+    socket.emit('all_options', allData);
+});
