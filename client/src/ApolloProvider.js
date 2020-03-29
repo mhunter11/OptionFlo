75468518f5b8fdd6eb2d10b7f21b9a5bb9796650
Mel <huntermelverton@gmail.com>
2020-03-29 14:59:15 -0400

    fixup: Add environment variables

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks'
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: process.env.REACT_GRAPHQL_URL,
 })
 
 const authLink = setContext(() => {
