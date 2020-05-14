097a559f9a7249e0e8a000bb952f8d13d11db8b2
Mel <huntermelverton@gmail.com>
2020-05-14 12:29:50 -0400

    fixup: Fix mobile card

--- client/src/pages/Homepage/LandingPage.js
+++ client/src/pages/Homepage/LandingPage.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import Typed from 'react-typed'
 
 import ImageBackground from './ImageBackground'
 import Benefit from './Benefit'
 import Affordable from './Affordable'
-
-import {
-  TYPING_ANIMATION_ARRAY,
-  CANCEL_ANYTIME,
-  START_TRIAL,
-} from './landingpage-data'
-
-import styles from './LandingPage.module.scss'
+import OurCommunity from './OurCommunity'
 
 export default function LandingPage() {
   return (
@@ -20,6 +11,7 @@ export default function LandingPage() {
       <ImageBackground />
       <Benefit />
       <Affordable />
+      <OurCommunity />
     </>
   )
 }
