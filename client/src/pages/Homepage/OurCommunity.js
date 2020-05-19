6aa31ec5b247291ca20e6658546b61143dee7706
Mel <huntermelverton@gmail.com>
2020-05-19 13:05:41 -0400

    fixup: Homepage semi done

--- client/src/pages/Homepage/OurCommunity.js
+++ client/src/pages/Homepage/OurCommunity.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Carousel from '../../components/Carousel'
-import cx from 'classnames'
 
 import {CUSTOMERS_FEEDBACK} from './community-data'
 
@@ -9,7 +8,14 @@ import styles from './OurCommunity.module.scss'
 export default function OurCommunity() {
   const OUR_COMMUNITY = 'Our community'
   const CUSTOMER_CARDS = CUSTOMERS_FEEDBACK.map((data, i) => {
-    return <div key={i}>{data.text}</div>
+    return (
+      <div key={i}>
+        <div className={styles.user_comment}>{data.text}</div>
+        <div className={styles.desktop_customer_name}>
+          <span>- {data.name}</span>
+        </div>
+      </div>
+    )
   })
   return (
     <div className={styles.community_container}>
@@ -20,7 +26,14 @@ export default function OurCommunity() {
         </div>
         <div className={styles.mobile_view}>
           {CUSTOMERS_FEEDBACK.map((data, i) => {
-            return <div key={i}>{data.name}</div>
+            return (
+              <div className={styles.mobile_container} key={i}>
+                <div className={styles.mobile_user_comment}>{data.text}</div>
+                <div className={styles.mobile_customer_name}>
+                  <span>- {data.name}</span>
+                </div>
+              </div>
+            )
           })}
         </div>
       </div>
--- client/src/pages/Homepage/community-data.js
+++ client/src/pages/Homepage/community-data.js
@@ -1,11 +1,11 @@
 export const CUSTOMERS_FEEDBACK = [
   {
     name: 'samdoc',
-    text: `Btw, to anyone considering the value of this DISCORD server - I was initially skeptical about the $25 value and even backed out the 3 day trial earlier. Then I looked around a option tracking services that cost $80-$100 a month. I work in an ICU and don't have time to look at a moniter most of the day and realized that what Jaime does by giving mobile alerts is awesome for the fee. Just my 2 cents`,
+    text: `To anyone considering the value of this DISCORD server - I was initially skeptical about the $25 value and even backed out the 3 day trial earlier. Then I looked around a option tracking services that cost $80-$100 a month. I work in an ICU and don't have time to look at a moniter most of the day and realized that what Jaime does by giving mobile alerts is awesome for the fee. Just my 2 cents`,
   },
   {
     name: 'Travis',
     text:
-      'yea that msft trade was spot on. Signed up for premium today. Paid for a years worth of membership on first alert. lol',
+      'Yea that msft trade was spot on. Signed up for premium today. Paid for a years worth of membership on first alert. lol',
   },
 ]
