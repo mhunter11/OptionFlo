1346dc7e13f6718cdf17a1fdea27f696bd6dbd85
Mel <huntermelverton@gmail.com>
2020-06-10 22:07:49 -0400

    fixup: Typo

--- client/src/pages/Homepage/community-data.js
+++ client/src/pages/Homepage/community-data.js
@@ -1,7 +1,7 @@
 export const CUSTOMERS_FEEDBACK = [
   {
     name: 'samdoc',
-    text: `To anyone considering the value of this DISCORD server - I was initially skeptical about the $25 value and even backed out the 3 day trial earlier. Then I looked around a option tracking services that cost $80-$100 a month. I work in an ICU and don't have time to look at a moniter most of the day and realized that what Jaime does by giving mobile alerts is awesome for the fee. Just my 2 cents`,
+    text: `To anyone considering the value of this DISCORD server - I was initially skeptical about the $25 value and even backed out the 3 day trial earlier. Then I looked around a option tracking services that cost $80-$100 a month. I work in an ICU and don't have time to look at a monitor most of the day and realized that what Jaime does by giving mobile alerts is awesome for the fee. Just my 2 cents`,
   },
   {
     name: 'Travis',
