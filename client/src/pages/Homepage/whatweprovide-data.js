f5dc646c9225ada38be979191f311a08ca4ffb43
Mel <huntermelverton@gmail.com>
2020-08-02 21:06:23 -0400

    fixup: Price changes

--- client/src/pages/Homepage/whatweprovide-data.js
+++ client/src/pages/Homepage/whatweprovide-data.js
@@ -6,14 +6,16 @@ export const OPTION_FEATURES = [
   'Realtime Option Order Flow',
   'Dark Pool Order Data',
   'Access to OptionFlo Discord',
-  'Access to our Top Traders',
-  'Option Alerts',
+  'Top Trader Alerts',
+  'Daily Twitter Highlights',
 ]
 
 export const OPTIONFLO = 'OptionFlo'
 
-export const PRICE = '$60'
+export const PRICE = 60
 
-export const PRICE_TEXT = `Price: ${PRICE}`
+export const CASH_PRICE = '$' + PRICE
+
+export const PRICE_TEXT = `Price: ${CASH_PRICE}`
 
 export const BUY_NOW = 'Buy Now'
