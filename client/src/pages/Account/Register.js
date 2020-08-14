34cfbd97b5557c47718c9cf857e4d0cbba07fb04
Mel <huntermelverton@gmail.com>
2020-08-14 15:37:14 -0400

    fixup: Add styling for Signin and Register

--- client/src/pages/Account/Register.js
+++ client/src/pages/Account/Register.js
@@ -1,14 +1,23 @@
 import React, {useContext, useState} from 'react'
-import {Button, Form} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import {Button} from 'semantic-ui-react'
 import {useMutation} from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import {Redirect} from 'react-router'
 
-import styles from './Register.module.scss'
+import styles from './Login.module.scss'
 
 import {FirebaseContext} from '../../context/auth'
 import {useForm} from '../../util/hooks'
 import swal from 'sweetalert'
+
+import {
+  WELCOME_BACK,
+  SIGN_IN_BUTTON,
+  SIGN_UP_UPDATES,
+  ALREADY_ACCOUNT,
+  SIGN_UP,
+} from './login-data'
 
 function Register(props) {
   const {firebase} = useContext(FirebaseContext)
@@ -57,20 +66,19 @@ function Register(props) {
       return
     }
 
-    firebase
-      .auth
+    firebase.auth
       .createUserWithEmailAndPassword(values.email, values.password)
       .then(function (data) {
         let user = data.user
         user.sendEmailVerification()
-        values.uid = user.uid;
+        values.uid = user.uid
         return user.updateProfile({
           displayName: values.username,
         })
       })
       .then(function () {
-        addUser();
-        firebase.auth.signOut();
+        addUser()
+        firebase.auth.signOut()
         swal(
           'Created',
           'Your account has been created\nPlease check your email for a verification link',
@@ -93,73 +101,84 @@ function Register(props) {
 
   return (
     <div className={styles.form_container}>
-      <Form
-        onSubmit={onSubmit}
-        noValidate
-      >
-        <h1>Register</h1>
-        <Form.Input
-          label="Username"
-          placeholder="Username.."
-          name="username"
-          type="text"
-          value={values.username}
-          error={errors.username ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Email"
-          placeholder="Email.."
-          name="email"
-          type="email"
-          value={values.email}
-          error={errors.email ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Password"
-          placeholder="Password.."
-          name="password"
-          type="password"
-          value={values.password}
-          error={errors.password ? true : false}
-          onChange={onChange}
-        />
-        <Form.Input
-          label="Confirm Password"
-          placeholder="Confirm Password.."
-          name="confirmPassword"
-          type="password"
-          value={values.confirmPassword}
-          error={errors.confirmPassword ? true : false}
-          onChange={onChange}
-        />
-        <Button type="submit" primary>
-          Register
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className="ui error message">
-          <ul className="list">
-            {Object.values(errors).map(value => (
-              <li key={value}>{value}</li>
-            ))}
-          </ul>
+      <div className={styles.container}>
+        <div className={styles.welcome_back_container}>
+          <h3 className={styles.h3}>{WELCOME_BACK}</h3>
+          <h5 className={styles.h5}>{SIGN_UP_UPDATES}</h5>
+          <h5 className={styles.h5}>{ALREADY_ACCOUNT}</h5>
+          <Link to="/login" className={styles.sign_up_button}>
+            {SIGN_IN_BUTTON}
+          </Link>
         </div>
-      )}
+        <div className={styles.form}>
+          <h2 className={styles.h2}>{SIGN_UP}</h2>
+          <form onSubmit={onSubmit} noValidate className={styles.form_submit}>
+            <div className={styles.input_container}>
+              <input
+                className={styles.input}
+                label="Username"
+                placeholder="Username.."
+                name="username"
+                type="text"
+                value={values.username}
+                error={errors.username ? true : false}
+                onChange={onChange}
+              />
+              <input
+                className={styles.input}
+                label="Email"
+                placeholder="Email.."
+                name="email"
+                type="email"
+                value={values.email}
+                error={errors.email ? true : false}
+                onChange={onChange}
+              />
+              <input
+                className={styles.input}
+                label="Password"
+                placeholder="Password.."
+                name="password"
+                type="password"
+                value={values.password}
+                error={errors.password ? true : false}
+                onChange={onChange}
+              />
+              <input
+                className={styles.input}
+                label="Confirm Password"
+                placeholder="Confirm Password.."
+                name="confirmPassword"
+                type="password"
+                value={values.confirmPassword}
+                error={errors.confirmPassword ? true : false}
+                onChange={onChange}
+              />
+            </div>
+            <div className={styles.submit_buttons}>
+              <Button type="submit" primary className={styles.primary}>
+                Sign Up
+              </Button>
+            </div>
+          </form>
+          {Object.keys(errors).length > 0 && (
+            <div className="ui error message">
+              <ul className="list">
+                {Object.values(errors).map(value => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 
 const REGISTER_USER = gql`
-  mutation register(
-    $uid: String!
-  ) {
-    register(
-      registerInput: {
-        uid: $uid
-      }
-    ) {
+  mutation register($uid: String!) {
+    register(registerInput: {uid: $uid}) {
       id
       email
       username
