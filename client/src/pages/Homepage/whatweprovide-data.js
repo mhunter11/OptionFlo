08518c878803ac257f2ef7459c80d334bfce493b
Mel <huntermelverton@gmail.com>
2020-09-08 00:54:19 -0400

    fixup: Add what we provide statement

--- client/src/pages/Homepage/whatweprovide-data.js
+++ client/src/pages/Homepage/whatweprovide-data.js
@@ -1,6 +1,7 @@
-export const PROVIDE_HEADER = 'What we provide'
+export const PROVIDE_HEADER = 'Why Us'
 
-export const PROVIDE_DESCRIPTION = 'Something....'
+export const PROVIDE_DESCRIPTION =
+  'The average person who works a full-time job and wants to invest does not typically have enough time in the day to read news articles,  research trade opportunities, and do technical analysis.  We simplify this process by aggregating all three into a single service within our Members Only Discord Server. Whether you are new to options trading or an intermediate trader, our community of skilled analysts and traders are here for you.'
 
 export const OPTION_FEATURES = [
   'Realtime Option Order Flow',
